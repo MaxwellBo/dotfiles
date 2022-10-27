b4de390fad23200190642d81479a03795923daa3
Max Bo <mjbo@canva.com>
2022-10-27 16:36:37 +1100

    Chnages

--- bash_profile
+++ bash_profile
@@ -7,3 +7,4 @@ export PAGER='less'
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 export PATH="$HOME/.cargo/bin:$PATH"
+if [ -e /Users/mjbo/.nix-profile/etc/profile.d/nix.sh ]; then . /Users/mjbo/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
--- setup.sh
+++ setup.sh
@@ -1,70 +1,35 @@
 ############
 # HOMEBREW #
 ############
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
 
-brew cask # initializes
+################
+# CORE EDITING # 
+################
 
-####################
-# LANGUAGE TOOLING #
-####################
-brew install --cask homebrew/cask-versions/adoptopenjdk8 
+brew install --cask iterm2 google-chrome spotify slack rectangle visual-studio-code jetbrains-toolbox nvim
 
-brew install yarn nvm haskell-stack scala sbt ammonite-repl leiningen nim pipenv go rustup-init python3
+defaults write -g ApplePressAndHoldEnabled -bool false
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-brew tap unisonweb/unison
-brew install unison-language
-
-# The Rust community should be shot for this
-# curl https://sh.rustup.rs -sSf | sh
-
-#############
-# DATABASES #
-#############
-brew install postgresql mysql
-
-docker run --name postgres -d -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres:9.6.1
-docker run --name maria -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mariadb:latest
-docker run --name redis -d redis
-
-# TODO: Check whether these utilities can read from stdin
-# psql -h localhost -p 5432 --username=postgres
-# sudo mysql --host=127.0.0.1 --user=root -p
-# docker run -it --link maria:mysql --rm mariadb sh -c 'exec mysql -h"$MYSQL_PORT_3306_TCP_ADDR" -P"$MYSQL_PORT_3306_TCP_PORT" -uroot -p"$MYSQL_ENV_MYSQL_ROOT_PASSWORD"'
-# CREATE DATABASE <DB_NAME>;
-
-
-#########
-# UTILS # 
-#########
-brew cask install iterm2 google-backup-and-sync google-chrome spotify slack wireshark spectacle thefuck
-
-#########
-# CLOUD #
-#########
-brew cask install heroku google-cloud-sdk
-
-#########
-# LATEX #
-#########
-brew cask install mactex
+nvim +PlugClean +PlugInstall +qa
 
 #######
 # CLI # 
 #######
-brew tap jesseduffield/lazydocker
-brew install nvim fd exa ccat prettyping ncdu ripgrep
-brew install pandoc gitless wget ranger screenfetch tmux lnav q jq fx gron trash tig m-cli entr fpp fzf fzy tokei httpie pup loc telnet diff-so-fancy htop ncdu bat prettyping shellcheck mosh googler up hadolint noti bsed lazydocker
+brew install fd exa ccat prettyping ncdu ripgrep
+brew install pandoc gitless wget ranger screenfetch tmux lnav q jq fx gron trash tig m-cli entr fpp fzf fzy tokei httpie pup loc telnet diff-so-fancy htop ncdu bat prettyping shellcheck mosh googler up hadolint noti bsed lazydocker thefuck
 
 #########
 # SHELL # 
 #########
-git clone --recursive https://github.com/MaxwellBo/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 chsh -s /bin/zsh
 
 # Reboot shell
-
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
@@ -75,22 +40,4 @@ done
 #########
 
 brew tap homebrew/cask-fonts
-
-brew cask install font-fira-code font-dejavu-sans-mono-for-powerline
-
-###########
-# EDITORS #
-###########
-
-defaults write -g ApplePressAndHoldEnabled -bool false
-
-brew install --cask visual-studio-code intellij-idea
-
-brew install neovim
-pip3 install neovim pyre-check flit pipenv
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-nvim +PlugClean +PlugInstall +qa
-
+brew install --cask font-fira-code font-dejavu-sans-mono-for-powerline
