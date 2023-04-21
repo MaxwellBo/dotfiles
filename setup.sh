92d32f100b63069a342930a7a2b4bcbea04ea527
Max Bo <mjbo@canva.com>
2023-04-21 15:24:26 +1000

    Updates

--- setup.sh
+++ setup.sh
@@ -4,11 +4,19 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install git
 
+#########
+# FONTS #
+#########
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code font-dejavu-sans-mono-for-powerline
+
 ################
 # CORE EDITING # 
 ################
 
-brew install --cask iterm2 google-chrome spotify slack rectangle visual-studio-code jetbrains-toolbox nvim
+brew install nvim
+brew install --cask iterm2 google-chrome spotify slack rectangle visual-studio-code jetbrains-toolbox 
 
 defaults write -g ApplePressAndHoldEnabled -bool false
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -19,6 +27,7 @@ nvim +PlugClean +PlugInstall +qa
 #######
 # CLI # 
 #######
+brew install nvim
 brew install fd exa ccat prettyping ncdu ripgrep
 brew install pandoc gitless wget ranger screenfetch tmux lnav q jq fx gron trash tig m-cli entr fpp fzf fzy tokei httpie pup loc telnet diff-so-fancy htop ncdu bat prettyping shellcheck mosh googler up hadolint noti bsed lazydocker thefuck
 
@@ -35,9 +44,3 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-#########
-# FONTS #
-#########
-
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code font-dejavu-sans-mono-for-powerline
